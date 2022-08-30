f1f59fe8ef22ed399085bf70658b2632cdeccac5
Gulbike Biyekenova <gbknva@gmail.com>
2022-08-30 11:13:40 -0400

    keys in elements

--- src/App.js
+++ src/App.js
@@ -7,24 +7,19 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: "Becca", lastName: "Biyekenova"},
-      company: "ABC"
+      monsters: [{ name: "Becca" }, { name: "Andrei" }, { name: "Kyle" }],
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, you work at {this.state.company}</p>
-          <button
-            onClick={() => {
-              this.setState({ name: {firstName: "Andrei" , lastName: "Nguen"}});
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster, index) => {
+          return (
+            <div key={index}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
